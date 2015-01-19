fbfe309949a3dd69c8a44d8466b4602e831b1486
chenjing <chenjing@chenjing-PC>
2015-01-19 18:49:52 +0800

    update

--- src/com/jingchen/autoload/PullToRefreshLayout.java
+++ src/com/jingchen/autoload/PullToRefreshLayout.java
@@ -113,10 +113,9 @@ public class PullToRefreshLayout extends RelativeLayout
 				if (state != REFRESHING)
 					changeState(INIT);
 				timer.cancel();
-				requestLayout();
 			}
 			// 刷新布局,会自动调用onLayout
-			if(pullDownY != 0)requestLayout();
+			requestLayout();
 		}
 
 	};
@@ -281,7 +280,7 @@ public class PullToRefreshLayout extends RelativeLayout
 			// 根据下拉距离改变比例
 			radio = (float) (2 + 2 * Math.tan(Math.PI / 2 / getMeasuredHeight()
 					* (pullDownY)));
-			if(pullDownY != 0)requestLayout();
+			requestLayout();
 			if (pullDownY <= refreshDist && state == RELEASE_TO_REFRESH)
 			{
 				// 如果下拉距离没达到刷新的距离且当前状态是释放刷新，改变状态为下拉刷新
